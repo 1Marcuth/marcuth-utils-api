ad150b11c71ddc2682ad42dc2f4ad05c44d14f2a
TantoFaz <thiago.playgames15@gmail.com>
2023-05-20 10:19:44 -0300

    ...

--- index.js
+++ index.js
@@ -1,17 +1,12 @@
 const express = require("express")
+const cors = require("cors")
 
 const scrapingRouter = require("./router/scraping/index.js")
 
 const port = process.env.PORT || 3001
 const app = express()
 
-app.use((request, resonse, next) => {
-    resonse.setHeader("Access-Control-Allow-Origin", "http://localhost")
-    resonse.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, PATCH, DELETE")
-    resonse.setHeader("Access-Control-Allow-Headers", "X-Requested-With,content-type")
-    resonse.setHeader("Access-Control-Allow-Credentials", true)
-    next()
-})
+app.use(cors({ origin: "*" }))
 
 app.use(express.json())
 app.use("/scraping", scrapingRouter)
