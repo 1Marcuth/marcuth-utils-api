5467b9b010b32f7f409c33a3d9d97d9dedc717e5
TantoFaz <thiago.playgames15@gmail.com>
2023-05-20 10:16:22 -0300

    corrigindo problema do cors

--- index.js
+++ index.js
@@ -5,6 +5,14 @@ const scrapingRouter = require("./router/scraping/index.js")
 const port = process.env.PORT || 3001
 const app = express()
 
+app.use((request, resonse, next) => {
+    resonse.setHeader("Access-Control-Allow-Origin", "http://localhost")
+    resonse.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, PATCH, DELETE")
+    resonse.setHeader("Access-Control-Allow-Headers", "X-Requested-With,content-type")
+    resonse.setHeader("Access-Control-Allow-Credentials", true)
+    next()
+})
+
 app.use(express.json())
 app.use("/scraping", scrapingRouter)
 
